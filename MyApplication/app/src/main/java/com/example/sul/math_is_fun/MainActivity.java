f863fc5af7278d3a486c703aef8788772a45c7d0
sulsaho <sulsaho@gmail.com>
2015-05-03 14:52:53 -0500

    created shared preferences as well as adapter for the class

--- MyApplication/app/src/main/java/com/example/sul/math_is_fun/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sul/math_is_fun/MainActivity.java
@@ -1,39 +1,28 @@
 package com.example.sul.math_is_fun;
 
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        // Fragment transaction for our Fragment -- very simple
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new RecyclerViewFragment())
+                        // Don't forget this nice little animation!
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit();
     }
 }
